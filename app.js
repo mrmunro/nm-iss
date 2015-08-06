e2757af96e4dc1fab4cdcaac406afa361d163979
Neil Munro <nmunro@gmail.com>
2015-08-07 01:42:19 +0200

    Included npm xml, trial attempt to create proxy xml

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs =require('fs');
+var xml = require('xml');
 
 console.log('started');
 
@@ -7,5 +8,21 @@ fs.readFile('./swagger/swagger.json', function(err,data) {
   
   var json = JSON.parse(data);
   console.log(json);
+  
+  console.log(json.paths);
+  
+  var pathXml;
+  
+  for(path in json.paths) {
+    //pathXml += xml([{Flow:[{_attr:{name:path.get.x-apigee-flow-name}}]}]);
+    var method = json.paths[path].get;
+    var name = method[0];
+    console.log(name);
+  }
+  
+  //console.log(pathXml);
+  
+  var test =  xml([ { ProxyEndpoint:[ {Description:json.info.description},{PreFlow : [{_attr:{name:'PreFlow'}}]}, {PostFlow : [{_attr:{name:'PostFlow'}}]} ]}], { declaration: true });
+  console.log(test);
   
 });

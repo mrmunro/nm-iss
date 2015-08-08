844f776578393a837bca41548f4401836e0884e7
Neil Munro <nmunro@gmail.com>
2015-08-08 12:24:44 +0200

    Update to add custom flows from swagger. YAML parsing attempt

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var fs =require('fs');
 var xml = require('xml');
+var yamljs = require('yamljs')
+
+
 
 console.log('started');
 
@@ -7,22 +10,36 @@ fs.readFile('./swagger/swagger.json', function(err,data) {
   if(err) {console.log(err);}
   
   var json = JSON.parse(data);
-  console.log(json);
+  //console.log(json);
   
-  console.log(json.paths);
+  //console.log(json.paths);
   
   var pathXml;
   
+  var proxy = [ { ProxyEndpoint:[ {Description:json.info.description},{PreFlow : [{_attr:{name:'PreFlow'}}]}]}];
+  
   for(path in json.paths) {
-    //pathXml += xml([{Flow:[{_attr:{name:path.get.x-apigee-flow-name}}]}]);
-    var method = json.paths[path].get;
-    var name = method[0];
-    console.log(name);
+    
+    proxy[0]["ProxyEndpoint"].push({Flow:[{_attr:{name:json.paths[path]["get"]["x-apigee-flow-name"]}}]});
+    
+    pathXml += xml([{Flow:[{_attr:{name:json.paths[path]["get"]["x-apigee-flow-name"]}}]}]);
+    var method = json.paths[path];
+    
+    
+    //console.log(method["get"]['x-apigee-flow-name']);
   }
   
-  //console.log(pathXml);
+  var postflow = {PostFlow : [{_attr:{name:'PostFlow'}}]};
+  proxy[0]["ProxyEndpoint"].push(postflow);
+  
+  console.log(xml(proxy,{ declaration: true }));
   
   var test =  xml([ { ProxyEndpoint:[ {Description:json.info.description},{PreFlow : [{_attr:{name:'PreFlow'}}]}, {PostFlow : [{_attr:{name:'PostFlow'}}]} ]}], { declaration: true });
-  console.log(test);
+ // console.log(test);
+  
+  /*var yamlJSON = yamljs.load("./yaml/swagger.yaml");
+  
+  console.log(yamlJSON);
+  console.log(yamlJSON['x-apigee-proxy-path']);*/
   
 });
